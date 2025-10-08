2df0ec593db1161a54c779f6f1c0218933bf01fa
Rafael Nicolas <nicolasrafael178@gmail.com>
2025-10-08 10:00:07 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,14 +37,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div className="site-wrapper">
-          <Header />
-          <main className="site-main">{children}</main>
-          <Footer />
-        </div>
-      </body>
-    </html>
+      <html lang="pt-BR">
+        <head>
+          <meta name="facebook-domain-verification" content="xijo57gdkycb3xtuo6nzg33r9bhqbb" />
+        </head>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <div className="site-wrapper">
+            <Header />
+            <main className="site-main">{children}</main>
+            <Footer />
+          </div>
+        </body>
+      </html>
   );
 }
